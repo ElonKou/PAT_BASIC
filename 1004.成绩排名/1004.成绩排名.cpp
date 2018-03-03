77b6717a4b00671113c3f402aba82bfac7a05bee
ElonKou <elonkou@ktime.cc>
2018-03-03 23:40:53 +0800

    Nothing.

---
+++ 1004.成绩排名/1004.成绩排名.cpp
@@ -0,0 +1,15 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// File:  1004.成绩排名.cpp
+// Author :elonkou
+// E-mail :elonkou@ktime.cc
+// Date   :Sat 03 Mar 2018 11:39:02 PM CST
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello,elonkou." << endl;
+    return 0;
+}
+
