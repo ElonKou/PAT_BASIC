b71a6acc9c028658c081b0e9191fbcf834a9cb24
ElonKou <elonkou@ktime.cc>
2018-03-04 22:01:48 +0800

    Accepted 1004.

--- 1004.成绩排名/1004.成绩排名.cpp
+++ 1004.成绩排名/1004.成绩排名.cpp
@@ -6,10 +6,34 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 
+struct stu{
+	string name;
+	string course;
+	int score;
+};
+
+int cmp(stu a,stu b){
+	return a.score > b.score;
+}
+
 int main(){
-    cout << "hello,elonkou." << endl;
-    return 0;
+	int cnt;
+	cin >> cnt;
+	stu stus[cnt];
+	for(int i = 0 ;i < cnt;i++){
+		cin >> stus[i].name
+			>> stus[i].course
+			>> stus[i].score;
+	}
+	sort(stus,stus + cnt,cmp);
+	cout << stus[0].name << " "
+		 << stus[0].course << endl
+		 << stus[cnt - 1].name << " "
+		 << stus[cnt - 1].course;
+	return 0;
 }
 
